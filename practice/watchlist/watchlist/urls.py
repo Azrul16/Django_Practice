0de829b6441e60f33e9d6434b175d298f23bff76
Azrul Amaline <azrul.amaline16@gmail.com>
2024-12-22 08:32:12 +0600

    creating api folder for DRF

--- practice/watchlist/watchlist/urls.py
+++ practice/watchlist/watchlist/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movies/', include('listitem_app.urls')),
+    path('movies/', include('listitem_app.api.urls')),
 ]
